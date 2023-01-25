9ce3f0e58236e300172982e26d10d14a0a0338ba
himTresor1 <amizeropeace@gmail.com>
2023-01-25 15:30:29 +0200

    Deployed universally

---
+++ components/fullLogo.tsx
@@ -0,0 +1,25 @@
+import { FC } from "react";
+const Logo: FC<{ animate?: boolean }> = ({ animate }) => {
+  return (
+    <div className="relative cursor-pointer group w-fit">
+      <img
+        src={"/favicond.png"}
+        alt="logo"
+        className={`group-hover:rotate-90 duration-700 delay-100 transition-transform ${
+          animate && " animate-spin"
+        }`}
+        style={{ width: !animate ? 75 : 130 }}
+      />
+      {/* <div
+        className={`absolute text-white ${
+          animate
+            ? "top-[calc(35%+3px)] text-4xl left-[calc(25%+6px)] "
+            : " top-[calc(30%+3px)] text-lg  left-[calc(25%+6px)] "
+        } font-[900]`}
+      >
+        ME
+      </div> */}
+    </div>
+  );
+};
+export default Logo;
---
+++ pages/_app.tsx
@@ -0,0 +1,63 @@
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
+import Logo from "@/components/fullLogo";
+import Navigation from "@/components/Navigation";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import Snowfall from "react-snowfall";
+import import { initializeGtm } from './gtm'
+
+
+export default function App({ Component, pageProps }: AppProps) {
+  const [snow, setSnow] = useState(false);
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    setSnow(true);
+    setTimeout(() => {
+      setShow(true);
+    }, 2000);
+  }, []);
+  if (!snow) return null;
+
+  return (
+    <>
+      <Head>
+        <title>Mehmet Emin</title>
+      </Head>
+      <Snowfall
+        style={{
+          position: "fixed",
+          width: "100vw",
+          height: "100vh",
+          zIndex: 50,
+        }}
+      />
+      <Snowfall
+        style={{
+          position: "fixed",
+          width: "100vw",
+          height: "100vh",
+          zIndex: 50,
+        }}
+      />
+      {!show ? (
+        <div className="h-screen w-full flex items-center justify-center">
+          <Logo animate />
+        </div>
+      ) : (
+        <>
+          <div className="min-h-screen overflow-x-hidden">
+            <div className=" max-w-[90rem] mx-auto px-5 xl:px-32">
+              <Navigation />
+              <Component {...pageProps} />
+            </div>
+            <Contact />
+          </div>
+          <Footer />
+        </>
+      )}
+    </>
+  );
+}
